78b4d9afb32e172cb1f57267c55fd023acd03ac6
Yaakov Aglamaz <yaakov.aglamaz@gmail.com>
2025-08-18 22:12:26 +0300

    clean debug

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,7 +12,6 @@ const PUBLIC_PATHS = [
 ];
 
 export async function middleware(request: NextRequest) {
-  console.log("in middleware");
   const path = request.nextUrl.pathname;
 
   if (PUBLIC_PATHS.some(p => path === p || path.startsWith(p + '/'))) {
@@ -32,17 +31,13 @@ export async function middleware(request: NextRequest) {
     if (path !== '/pending-member') {
       const siteId = process.env.NEXT_SITE_ID;
       const memberRes = await apiFetchFromMiddlewareJSON(request, `/api/user/member-info?siteId=${siteId}`);
-      console.log(memberRes);
       if (!(memberRes.success && memberRes.member && ['member', 'admin'].includes(memberRes.member.role))) {
-        console.log("failed", memberRes)
         return NextResponse.redirect(new URL('/pending-member', request.url));
       }
     }
   } catch (error) {
-    console.error(`middleware error, accessing ${path}`, error);
     return NextResponse.redirect(new URL('/login', request.url));
   }
-  console.log("we are cool");
   return NextResponse.next();
 }
 
