c2cb27bd83f04d1be856e42f96651f829719a009
Yaakov Aglamaz <yaakov.aglamaz@gmail.com>
2025-08-18 20:24:31 +0300

    fix build error

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const path = require('path');
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
+    missingSuspenseWithCSRBailout: false,
+
    },
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname, 'src');
